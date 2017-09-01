6f6e4f3a7138dc1f76301c21a85503199e254f83
NetoPedro <pedroneto_09@hotmail.com>
2017-09-01 16:53:46 +0100

    Some minor changes

--- ArmisHandballiOS/TournamentViewController.swift
+++ ArmisHandballiOS/TournamentViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class MatchDaysViewController: UIViewController {
+class TournamentViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
